435d5c771be3f1060f830424b25000291771e1a8
falcon1641 <94806141+falcon1641@users.noreply.github.com>
2022-01-10 19:37:25 -0500

    adding server script to manifest
    added server script to the manifest

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,8 +1,10 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Arduous Gamer / Demented Dude'
-description 'An awesome, but short, description'
+author 'Converted to qb by falcon1641. This is a fork from ArduousDev standalone tintchecker'
+description 'device to check vehicle tint legality.'
 version '1.0.0'
 
 client_script 'client.lua'
+
+server_script 'server.lua'
